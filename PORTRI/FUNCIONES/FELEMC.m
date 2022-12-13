3b3b920b4e5fb1d1a41db97c8de3cd54a5cfe605
Martin Estrada <mestradam@unal.edu.co>
2022-12-13 10:20:59 -0500

    lgoritmo principal 2D no lineal

--- PORTRI/FUNCIONES/FELEMC.m
+++
@@ -1,7 +0,0 @@
-% Vector de fuerzas equivalentes a la carga distribuida uniforme
-% en dirección z-global y positiva hacia abajo.
-% PENDIENTE 
-function [FEL] = FELEMC(XYE,WZGL)
-
- 
-end
--- TPRGID/PEFICA-O.gid/PEFICA-O.win.bat
+++
@@ -1,3 +0,0 @@
-copy %1.dat %1.m
-erase %1.dat
-rem %3\comet.exe %1
